cecd6a337451823ff5e1832de880fe35f6de66ac
ajkmonster <phimphachanh.momo.victor@gmail.com>
2019-01-30 15:45:55 -0500

    Initial commit

---
+++ src/FizzBuzz.java
@@ -0,0 +1,17 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        int x;
+        for (x=1; x<=100; x++){
+            if (x%3==0){
+                System.out.println("Fizz");
+            }else if (x%5==0)
+                System.out.println("Buzz");
+            else if (x%3==0 && x%5==0) {
+                System.out.println("FizzBuzz");
+            }
+            else {
+                System.out.println(x);
+            }
+        }
+    }
+}
