94600c3602d93c055d13abbb315010b9c91bbcf9
ajkmonster <phimphachanh.momo.victor@gmail.com>
2019-01-31 09:47:11 -0500

    change in if else order sequence

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,12 +2,12 @@ public class FizzBuzz {
     public static void main(String[] args) {
         int x;
         for (x=1; x<=100; x++){
-            if (x%3==0){
-                System.out.println("Fizz");
+            if (x%3==0 && x%5==0){
+                System.out.println("FizzBuzz");
             }else if (x%5==0)
                 System.out.println("Buzz");
-            else if (x%3==0 && x%5==0) {
-                System.out.println("FizzBuzz");
+            else if (x%3==0) {
+                System.out.println("Fizz");
             }
             else {
                 System.out.println(x);
